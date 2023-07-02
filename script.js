85586b876a648368c2e7b202ea088ea3a178dd1c
Maroosh-Gillani <76667567+Maroosh-Gillani@users.noreply.github.com>
2023-07-02 01:46:46 -0400

    Added R,P,S buttons, game works in console log.

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
-function getComputerChoice() {
-    compChoice = ["Rock", "Paper", "Scissors"];
-    randNum = Math.floor(Math.random() * 3);
-    return compChoice[randNum];
-}
+document.addEventListener('DOMContentLoaded', function () { // Had issues without making sure DOM content was loaded first.
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
-        return "Tie";
-    }
-    else if (playerSelection.toUpperCase() == "ROCK") {
-        if (computerSelection.toUpperCase() == "SCISSORS") {
-            return "You won! Rock beats Scissors.";
-        }
-        else {
-            return "You lost. Paper beats Rock.";
-        }
-    }
-    else if (playerSelection.toUpperCase() == "PAPER") {
-        if (computerSelection.toUpperCase() == "ROCK") {
-            return "You won! Paper beats Rock.";
-        }
-        else {
-            return "You lost. Scissors beat Paper.";
-        }
-    }
-    else if (playerSelection.toUpperCase() == "SCISSORS") {
-        if (computerSelection.toUpperCase() == "PAPER") {
-            return "You won! Scissors beat Paper.";
-        }
-        else {
-            return "You lost. Rock beats Scissors.";
-        }
-    }
-}
+    const buttons = document.querySelectorAll('button');
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+    // Clicking the buttons
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const playerChoice = this.value;
+            const computerChoice = getComputerChoice();
+            console.log(computerChoice);
+
+            const result = playRound(playerChoice, computerChoice);
+            console.log(result);
+        });
+    });
+
+
+    function getComputerChoice() {
+        compChoice = ["Rock", "Paper", "Scissors"];
+        randNum = Math.floor(Math.random() * 3);
+        return compChoice[randNum];
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
+            return "Tie";
+        }
+        else if (playerSelection.toUpperCase() == "ROCK") {
+            if (computerSelection.toUpperCase() == "SCISSORS") {
+                return "You won! Rock beats Scissors.";
+            }
+            else {
+                return "You lost. Paper beats Rock.";
+            }
+        }
+        else if (playerSelection.toUpperCase() == "PAPER") {
+            if (computerSelection.toUpperCase() == "ROCK") {
+                return "You won! Paper beats Rock.";
+            }
+            else {
+                return "You lost. Scissors beat Paper.";
+            }
+        }
+        else if (playerSelection.toUpperCase() == "SCISSORS") {
+            if (computerSelection.toUpperCase() == "PAPER") {
+                return "You won! Scissors beat Paper.";
+            }
+            else {
+                return "You lost. Rock beats Scissors.";
+            }
+        }
+    }
+
+    function game() {
         let userSelection = prompt("Choose from Rock, Paper, & Scissors.");
         let compSelection = getComputerChoice();
         console.log("The computer chose: " + compSelection);
         console.log(playRound(userSelection, compSelection));
     }
-}
+
+});
