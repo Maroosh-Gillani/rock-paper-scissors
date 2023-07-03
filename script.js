d4956fc814ba50c294e00b8da925ff71cb760cd2
Maroosh-Gillani <76667567+Maroosh-Gillani@users.noreply.github.com>
2023-07-03 01:14:03 -0400

    Game plays for 5 rounds with working reset button. Just need to display win/loss message before pressing buttons the 6th time.

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () { // Had issues without making sure DOM content was loaded first.
+    let playerScore = 0;
+    let computerScore = 0;
 
-    const buttons = document.querySelectorAll('button');
-
+    const buttons = document.querySelectorAll('.rpsButton');
     const resultsDiv = document.querySelector('.results');
+    const resetButton = document.querySelector('.resetButton');
+    const resultPara = document.createElement('p');
 
-    // Clicking the buttons
+    // let resetGame = false;
+    
+    // Clicking the rock, paper, scissors buttons
     buttons.forEach(button => {
         button.addEventListener('click', function () {
-            const playerChoice = this.value;
-            const computerChoice = getComputerChoice();
-            console.log(computerChoice);
+            if (playerScore < 5 && computerScore < 5) {
+                const playerChoice = this.value;
+                const computerChoice = getComputerChoice();
+                console.log(computerChoice);
 
-            const result = playRound(playerChoice, computerChoice);
-            console.log(result);
+                const result = playRound(playerChoice, computerChoice);
+                // playFiveRounds(playerChoice, computerChoice);
+                console.log(playerScore);
+                console.log(computerScore);
+            }
+            else if (playerScore === 5 || computerScore === 5) {
+                if (playerScore === 5) {
+                    resultPara.textContent = "You won the game!";
+                }
+                else {
+                    resultPara.textContent = "You lost.";
+                }
+                resultsDiv.appendChild(resultPara);
+            }
         });
+    });
+
+    //clicking the reset button
+    resetButton.addEventListener('click', function () {
+        playerScore = 0;
+        computerScore = 0;
+        resultPara.textContent = "";
+        resultsDiv.removeChild(resultPara);
     });
 
 
@@ -24,51 +50,59 @@ document.addEventListener('DOMContentLoaded', function () { // Had issues withou
     }
 
     function playRound(playerSelection, computerSelection) {
-
-        const resultPara = document.createElement('p');
-
-
         if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
             resultPara.textContent = "Tie";
-            resultsDiv.appendChild(resultPara);
         }
         else if (playerSelection.toUpperCase() == "ROCK") {
             if (computerSelection.toUpperCase() == "SCISSORS") {
                 resultPara.textContent = `You won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`;
-                resultsDiv.appendChild(resultPara); 
+                playerScore ++;
             }
             else {
                 resultPara.textContent = `You lost. ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`;
-                resultsDiv.appendChild(resultPara);
+                computerScore ++;
             }
         }
         else if (playerSelection.toUpperCase() == "PAPER") {
             if (computerSelection.toUpperCase() == "ROCK") {
                 resultPara.textContent = `You won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`;
-                resultsDiv.appendChild(resultPara);
+                playerScore ++;
             }
             else {
                 resultPara.textContent = `You lost. ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`;
-                resultsDiv.appendChild(resultPara);
+                computerScore ++;
             }
         }
         else if (playerSelection.toUpperCase() == "SCISSORS") {
             if (computerSelection.toUpperCase() == "PAPER") {
                 resultPara.textContent = `You won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`;
-                resultsDiv.appendChild(resultPara);
+                playerScore ++;
             }
             else {
                 resultPara.textContent = `You lost. ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`;
-                resultsDiv.appendChild(resultPara);
+                computerScore ++;
             }
         }
+        resultsDiv.appendChild(resultPara);
     }
 
-    function game() {
-        let userSelection = prompt("Choose from Rock, Paper, & Scissors.");
-        let compSelection = getComputerChoice();
-        console.log("The computer chose: " + compSelection);
-        console.log(playRound(userSelection, compSelection));
-    }
+    // function reset() {
+    //     resetGame = true;
+    //     playerScore = 0;
+    //     computerScore = 0;
+    // }
+
+    // function game(playerSelection, computerSelection) {
+    //     let userSelection = prompt("Choose from Rock, Paper, & Scissors.");
+    //     let compSelection = getComputerChoice();
+    //     console.log("The computer chose: " + compSelection);
+    //     console.log(playRound(userSelection, compSelection));
+    // }
+
+    // function playFiveRounds(playerSelection, computerSelection) {
+    //     while (playerScore < 5 || computerScore < 5) {
+    //         playRound(playerSelection, computerSelection);
+    //     }
+    // }
 
 });
